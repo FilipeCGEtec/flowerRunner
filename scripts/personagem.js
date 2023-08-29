ebac2eabbe02f15a135a6eaba6650d056d4877b6
Filipe2JD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-08-29 10:52:57 -0300

    criação da classe

---
+++ scripts/personagem.js
@@ -0,0 +1,9 @@
+class Personagem {
+  constructor(posicao, velocidade, posicaoY, numeroDeFlores){
+    this.posicao = posicaoX;
+    this.velociadade = vel;
+    this.posicaoY = posY;
+    this.numeroDeFlores = numFlores;
+  }
+
+}
