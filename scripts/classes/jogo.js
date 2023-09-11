8c645150f2511ae4497cd578b6861108849c09ab
filipe <filipe.goncalves29@etec.sp.gov.br>
2023-09-11 17:04:45 -0300

    Criação das demais classes e atualização do script

---
+++ scripts/classes/jogo.js
@@ -0,0 +1,18 @@
+class Jogo {
+    constructor(flores, estadoDeJogo){
+      this.flores = flores;
+      this.estadoDejogo = estadodeJogo;
+    }
+    IniciarJogo(){
+      
+    }
+    FinalizarJogo(){
+      
+    }
+    DesenharCenario(){
+
+    }
+    AtualizarPotuacao(){
+        
+    }
+  }
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-.
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "blue"
+ctx.fillRect(0, (canvas.height / 1.3), canvas.width, (canvas.height / 1.2));
+
+
+
